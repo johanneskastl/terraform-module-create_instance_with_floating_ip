2d9b0c72e2d16d4e2f2da1a2acca1d5f7526a3a6
Johannes Kastl <kastl@b1-systems.de>
2019-11-29 22:50:20 +0100

    fix wrong syntax for port and instance names

--- 20_create_network_port.tf
+++ 20_create_network_port.tf
@@ -2,7 +2,7 @@
 
 resource "openstack_networking_port_v2" "instance_ports" {
   count              = var.desired_number
-  name               = "$ {var.port_name_or_prefix}[count.index + 1]}"
+  name               = "${var.port_name_or_prefix}${count.index + 1}"
   network_id         = var.port_network_id
   admin_state_up     = var.port_admin_state_up
   security_group_ids = var.port_security_group_ids
