53ea2126b2cfab894b335f5c8a9bae58e3c938f3
Johannes Kastl <kastl@b1-systems.de>
2019-11-29 23:01:24 +0100

    REFACTOR: prefix name suffixes (client1) with a leading zero (client01), if needed

--- 30_create_instance.tf
+++ 30_create_instance.tf
@@ -2,7 +2,7 @@
 
 resource "openstack_compute_instance_v2" "instances" {
   count           = var.desired_number
-  name            = "${var.instance_name_or_prefix}${count.index + 1}"
+  name            = "${var.instance_name_or_prefix}${format("%02d", count.index + 1)}"
   flavor_name     = var.instance_flavor_name
   image_name      = var.instance_image_name
   user_data       = var.instance_user_data
